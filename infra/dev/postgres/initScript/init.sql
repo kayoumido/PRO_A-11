43f755c4508292e75d380d83150370743bed5daa
sacha <sacha.perdrizat@gmail.com>
2020-03-26 11:57:23 +0100

    #5-add dbcred to .env file

--- infra/dev/postgres/initScript/init.sql
+++ infra/dev/postgres/initScript/init.sql
@@ -1,8 +1,6 @@
-CREATE DATABASE example;
+CREATE DATABASE laravel;
 
-USE example;
-
-CREATE TABLE employees (
+CREATE TABLE employee (
     employee_id   NUMERIC       NOT NULL,
     first_name    VARCHAR(1000) NOT NULL,
     last_name     VARCHAR(900)  NOT NULL,
