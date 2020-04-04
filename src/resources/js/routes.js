355fd52ff864f30b0dde6909e1b28e6615ef3b03
Alban <you@example.com>
2020-04-04 22:06:01 +0200

    #34 first draft of the Authentication vue

--- src/resources/js/routes.js
+++ src/resources/js/routes.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Hello from './components/Hello';
+import Authentication from './components/Authentication';
 
 Vue.use(VueRouter);
 
@@ -11,5 +12,10 @@ export const router = new VueRouter({
         path: '/hello',
         name: 'Hello',
         component: Hello
-    }],
+    },
+    {
+        path: '/authentication',
+        name: 'Authentication',
+        component: Authentication
+  }],
 });
