6fb6dc17305a290d2017d2561043a90b621f434b
sacha <sacha.perdrizat@gmail.com>
2020-04-07 09:15:12 +0200

    #34 setup Laravel ui and Vue auth view

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,6 +1,23 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
 
-import Vue from 'vue';
+window.Vue = require('vue');
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 import {router} from './routes';
 import App from './App.vue';
@@ -12,9 +29,6 @@ if (process.env.NODE_ENV === "development") {
 }
 
 
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  render: h => h(App),
+const app = new Vue({
+    el: '#app',
 });
