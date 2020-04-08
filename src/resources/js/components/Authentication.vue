51a1c008d200a19261288b3c02f1d1b58e10899f
sacha <sacha.perdrizat@gmail.com>
2020-04-08 12:16:10 +0200

    #34-add-post-send-WIP

--- src/resources/js/components/Authentication.vue
+++ src/resources/js/components/Authentication.vue
@@ -2,24 +2,30 @@
   <div id="app">
   
     <p class="title">PAUL</p>
-
-    <input type="text" name="username" v-model="input.username" placeholder="Nom d'utilisateur" />
-    <input type="password" name="password" v-model="input.password" placeholder="Mot de passe" />
-    <button type="button" v-on:click="login()">Se connecter</button>
-    <p v-if="isBlank"> Pas de données</p>
-    <p v-if="isIncorect"> Nom ou mot de passe incorrect</p>
+    <form>
+      <input type="text" name="username" v-model="input.username" placeholder="Nom d'utilisateur" />
+      <input type="password" name="password" v-model="input.password" placeholder="Mot de passe" />
+      <button type="button" v-on:click="login()">Se connecter</button>
+      
+      <p v-if="isBlank"> Pas de données</p>
+      <p v-if="isIncorect"> Nom ou mot de passe incorrect</p>
+    </form>
+    
 
   </div>
 </template>
 
 <script>
+    import axios from 'axios'
+    import router from '../routes'
+
     export default {
     name : 'Authentication',
     data () {
         return{
           input: {
-              username: "",
-              password: ""
+              username: '',
+              password: ''
           },
       darkBlue: '#5F74E8',
       lightBlue: '#69A7FF',
@@ -31,6 +37,21 @@
       login() {
          if(this.input.username != "" && this.input.password != "") { //if not blank
             //sends credentials to backend for verification
+
+            axios.post(
+              '/login',
+              this.$input // to reformat if needed
+            )
+            .then(function (data){
+              // this function is launched after the post request
+              console.log(data);
+
+              
+            })
+            .catch(function (error){
+              console.log(error);
+            });
+
          } else {
            this.isBlank=true;
          }
