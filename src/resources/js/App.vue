b36fd12dcdb2ee5a7c55d593320329a885936813
Alban <you@example.com>
2020-04-10 22:01:46 +0200

    #34 app remembers login

--- src/resources/js/App.vue
+++ src/resources/js/App.vue
@@ -1,18 +1,36 @@
 <template>
     <div id="app" class="container">
+        <div class="container">
+            <p v-if="isLogged">{{username}}</p>
+        </div>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="collapse navbar-collapse">
                 <div class="navbar-nav">
                     <router-link to="/hello" class="nav-item nav-link">Home</router-link>
-                    <router-link to="/authentication" class="nav-item nav-link">Authentication</router-link>
+                    <router-link to="/authentication" class="nav-item nav-link" >Authentication</router-link>
                 </div>
             </div>
         </nav>
         <br/>
-        <router-view></router-view>
+        <router-view @login-successfull="updateLogin"></router-view>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        name: 'App',
+        data () {
+            return{
+                username: "",
+                isLogged: false
+            }
+
+        },
+        methods:{
+            updateLogin(username){
+                this.username=username;
+                this.isLogged =true;
+            }
+        }
+    }
 </script>
