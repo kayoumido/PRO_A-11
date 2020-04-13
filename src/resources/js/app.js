5d55b7071bf1779f5c1230b5b45c81379649f3fa
Sachouille <sacha.perdrizat@gmail.com>
2020-04-13 23:05:36 +0200

    #33-add-mirageJs-feature

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,17 +1,6 @@
 require('./bootstrap');
 
-window.Vue = require('vue');
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+import Vue from 'vue';
 
 import {router} from './routes';
 import App from './App.vue';
@@ -21,8 +10,6 @@ import makeServer from './api';
 if (process.env.NODE_ENV === "development") {
   makeServer();
 }
-
-
 
 new Vue({
   el: '#app',
--- src/resources/js/components/AccountEdition.vue
+++ src/resources/js/components/AccountEdition.vue
@@ -1,42 +1,46 @@
 <template>
   <div class="container">
     <h2>My Account</h2>
-    
-    <div v-if="isBlank" class="error">
+
+    <div v-if="emptySubmit" class="error">
       <h3>you cannot submit empty change</h3>
     </div>
-    
+
     <div v-if="errorHttp" class="error">
       <h3>error while sending data</h3>
     </div>
 
-    <form @submit.prevent="updateUsername()">
-   
+    <form @submit.prevent="updateFirstName()">
       <label for="updateUsername">
-        <input v-model="newUserName" type="text" placeholder="New username" required />
+        <input v-model="newFirstName" type="text" :placeholder="this.User.fname" />
       </label>
 
-      <input type="submit" value="update username" />
+      <input type="submit" value="update firstname" />
+    </form>
+
+    <form @submit.prevent="updateLastName()">
+      <label for="updateUsername">
+        <input v-model="newLastName" type="text" :placeholder="this.User.lname" />
+      </label>
+
+      <input type="submit" value="update lastname" />
     </form>
 
     <form @submit.prevent="updateEmail()">
-   
       <label for="updateEmail">
-        <input v-model="newEmail" type="email" placeholder="New E-mail" required />
+        <input v-model="newEmail" type="email" :placeholder="this.User.email" />
       </label>
 
       <input type="submit" value="update email" />
     </form>
 
     <form @submit.prevent="updatePassword()">
-  
       <label for="updatePassword">
-        <input v-model="newPassword" type="password" placeholder="New Password" required />
+        <input v-model="newPassword" type="password" placeholder="New Password" />
       </label>
 
       <input type="submit" value="update password" />
     </form>
-
   </div>
 </template>
 
@@ -46,71 +50,96 @@ import router from "../routes";
 
 export default {
   name: "AccountEdition",
-  props: ['parent'],
+  props: ["parent"],
   data() {
     return {
-      currentUsername : 'PaulHochon', // will use parent data when available
-      newUserName: '',
+      // will use parent data when available (eg. parent.data.user)
+      User: {
+        id: 3,
+        fname: "Jane",
+        lname: "Doe",
+        email: "jane@paul.lo"
+      },
+      newFirstName: '',
+      newLastName: '',
       newPassword: '',
       newEmail: '',
-      isBlank : false,
-      errorHttp : false
+      emptySubmit: false,
+      errorHttp: false
     };
   },
   methods: {
-    submitChange(data){
-      // send http request with axios and catch response or error 
-      axios.put("/v1/api/user",data)
-        .then((response) => {
-            console.log(response);
-
-            if (data.newUsername != null){
-              // if username has been update we adapte the CurrentUsername
-              this.currentUsername = data.newUsername; 
-            }
-
+    submitChange(data) {
+      const apiUrl = "/api/user/" + this.User.id;
+      // send http request with axios and catch response or error
+      axios
+        .put(apiUrl, data)
+        .then(response => {
+          console.log(response);
+          this.User = response.data.user;
         })
-        .catch((errorResponse) => {
-            this.errorHttp = true;
-            console.log(errorResponse);
+        .catch(errorResponse => {
+          this.errorHttp = true;
+          console.log(errorResponse);
+        });
+    },
+
+    isEmpty(textInput) {
+      // if (textInput === "") {
+      //   this.emptySubmit = true;
+      // } else {
+      //   this.emptySubmit = false;
+      // }
+
+      this.emptySubmit = (textInput === "");
+
+      return this.emptySubmit;
+      
+    },
+
+    // call the submit function according to the data that the user would change
+
+    updateFirstName() {
+      if (!this.isEmpty(this.newFirstName)) {
+
+        this.submitChange({
+          fname: this.newFirstName
+        });
+        this.newFirstName = "";
+      }
+    },
+    updateLastName() {
+      if (!this.isEmpty(this.newLastName)) {
+
+        this.submitChange({
+          lname: this.newLastName
+        });
+        // clean the form
+        this.newLastName = "";
+      }
+    },
+
+    updateEmail() {
+      if (!this.isEmpty(this.newEmail)) {
+
+        this.submitChange({
+          email: this.newEmail
         });
 
-    },
-    
-    // call the submit function according to the data that the user would change
-     
-    updateUsername() {
-      if(this.newUserName !== ''){
-        this.submitChange({
-          name : this.currentUsername,
-          newUsername : this.newUserName
-        });
-      }else{
-        isBlank = true;
-      }
-        
-    },
-    updateEmail() {
-      if(this.newEmail !== ''){
-        this.submitChange({
-          name : this.currentUsername,
-          newEmail : this.newEmail
-        });
-      }else{
-        isBlank = true;
+        // clean the form
+        this.newEmail = "";
       }
     },
     updatePassword() {
-      if(this.newPassword !== ''){
+      if (!this.isEmpty(this.newPassword)) {
+
         this.submitChange({
-          name : this.currentUsername,
-          newPassword : this.newPassword
+          password: this.newPassword
         });
-      }else{
-        isBlank = true;
+        // clean the form
+        this.newPassword = "";
       }
     }
-    
   }
 };
 </script>
