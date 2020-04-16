91199d4a9b561dd7a892b73ac3655652f5c53386
Sachouille <sacha.perdrizat@gmail.com>
2020-04-16 14:23:22 +0200

    #90-simple-error-message

--- src/resources/js/components/Presentation.vue
+++ src/resources/js/components/Presentation.vue
@@ -1,70 +1,48 @@
 <template>
-     <div class="container">
-          <div v-if="isError" class="error">
-               <h3> {{ message }} </h3>
-               <div v-for="(value, name) in errorResponse" :key="name">
-                    {{ name }} : {{ value }}
-               </div>
-          </div>
+  <div class="container">
+    <div v-if="isError" class="error">
+      <h3>{{ message }}</h3>
+    </div>
 
-          <h3>Presentation info</h3>
-     
-          <div v-for="(value, name) in presentation" :key="name">
-               {{ name }} : {{ value }}
-          </div>
-     
-          <h3>Polls related</h3>
+    <h3>Presentation info</h3>
 
-          <div v-for="poll in polls" :key="poll.id">
-               {{ poll.id }} : {{ poll.subject }}
-          </div>
-
-     </div>
-
+    <div v-for="(value, name) in presentation" :key="name">{{ name }} : {{ value }}</div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-     name : 'Presentation',
-     props : ['idPresentation'],
-     data() {
-          return {
-               presentation : {},
-               polls : [],
-               isError : false,
-               message : '',
-          }
-     },
-     beforeMount () {
-          this.getPresentationInfo();
-     },
-     methods : {
-          getPresentationInfo() {
-               
-               //const id = idPresentation;
-               const id = 1;
-               const apiUrl = 'api/v1/presentations/aaa' + id;
-               
-               axios.get(apiUrl)
-                    .then((response) =>{
-                         this.presentation = response.data.data;
-                    })
-                    .catch((error) => {
-                         this.isError = true;
-                         this.message = 'error while sending data : ' + error.response.status;
+  name: "Presentation",
+  props: ["idPresentation"], // we will send the id of the presentation through props
+  data() {
+    return {
+      presentation: {},
+      polls: [],
+      isError: false,
+      message: ""
+    };
+  },
+  beforeMount() {
+    this.getPresentationInfo();
+  },
+  methods: {
+    getPresentationInfo() {
+      //const id = idPresentation;
+      const id = 1;
+      const apiUrl = "api/v1/presentations/aaa" + id;
 
-                    })
-
-          },
-          getPollsRelated() { 
-               // related to #112
-               
-          }
-
-     }
-
-
-}
+      axios
+        .get(apiUrl)
+        .then(response => {
+          this.presentation = response.data.data;
+        })
+        .catch(error => {
+          this.isError = true;
+          this.message = "an error has occured while sending the data";
+        });
+    }
+  }
+};
 </script>
