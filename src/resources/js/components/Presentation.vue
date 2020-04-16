f20e9af1653d928b6cbaecfc76f0ef29daa8a19b
Sachouille <sacha.perdrizat@gmail.com>
2020-04-16 11:27:48 +0200

    #90-add-error-message

--- src/resources/js/components/Presentation.vue
+++ src/resources/js/components/Presentation.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="container">
+     <div class="container">
+          <div v-if="isError" class="error">
+               <h3> {{ message }} </h3>
+               <div v-for="(value, name) in errorResponse" :key="name">
+                    {{ name }} : {{ value }}
+               </div>
+          </div>
 
-     <div v-for="(value, name) in presentation" :key="name">
-          {{ name }} : {{ value }}
+          <h3>Presentation info</h3>
+     
+          <div v-for="(value, name) in presentation" :key="name">
+               {{ name }} : {{ value }}
+          </div>
+     
+          <h3>Polls related</h3>
+
+          <div v-for="poll in polls" :key="poll.id">
+               {{ poll.id }} : {{ poll.subject }}
+          </div>
+
      </div>
-
-  </div>
 
 </template>
 
@@ -17,26 +31,38 @@ export default {
      props : ['idPresentation'],
      data() {
           return {
-               presentation : {}
+               presentation : {},
+               polls : [],
+               isError : false,
+               message : '',
           }
      },
      beforeMount () {
           this.getPresentationInfo();
      },
      methods : {
-          getPresentationInfo(){
-
-               const apiUrl = 'api/v1/presentations/1' /*idPresentation*/;
+          getPresentationInfo() {
+               
+               //const id = idPresentation;
+               const id = 1;
+               const apiUrl = 'api/v1/presentations/aaa' + id;
                
                axios.get(apiUrl)
                     .then((response) =>{
                          this.presentation = response.data.data;
                     })
                     .catch((error) => {
-                         console.log(error);
+                         this.isError = true;
+                         this.message = 'error while sending data : ' + error.response.status;
+
                     })
 
+          },
+          getPollsRelated() { 
+               // related to #112
+               
           }
+
      }
 
 
