c3358c1cbe729ec7e4c9865d3a9c0392606f8b40
Sachouille <sacha.perdrizat@gmail.com>
2020-04-16 14:07:08 +0200

    Revert "#90-rm-unwanted-files"
    This reverts commit 8f294d9abf87231aeda2286e6212015a8357d25d.

---
+++ src/resources/js/api/index.js
@@ -0,0 +1,45 @@
+import { Server, Model } from 'miragejs';
+
+export default function makeServer({ environment = 'development' } = {}) {
+  const server = new Server({
+    environment,
+    models: {
+      user: Model,
+    },
+
+    seeds(srv) {
+      srv.create('user', {
+        id: 1,
+        fname: 'Shawn',
+        lname: 'The Sheep',
+        email: 'shaw@paul.lo',
+      });
+      srv.create('user', {
+        id: 2,
+        fname: 'John',
+        lname: 'Doe',
+        email: 'john@paul.lo',
+      });
+      srv.create('user', {
+        id: 3,
+        fname: 'Jane',
+        lname: 'Doe',
+        email: 'jane@paul.lo',
+      });
+    },
+
+    routes() {
+      this.namespace = 'api';
+
+      this.get('/user/:id', (schema, request) => {
+        const id = request.params.id;
+
+        return schema.users.find(id);
+      });
+
+      this.passthrough();
+    },
+  });
+
+  return server;
+}
