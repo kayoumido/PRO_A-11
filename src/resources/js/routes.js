804720726fa7b185423e9ecb94eed137724e5cad
Sachouille <sacha.perdrizat@gmail.com>
2020-04-21 22:38:33 +0200

    #36 add route to component

--- src/resources/js/routes.js
+++ src/resources/js/routes.js
@@ -1,15 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import Hello from './components/Hello';
+import presentations from './components/ListPresentations';
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
   mode: 'history',
-    routes: [{
-        path: '/hello',
-        name: 'Hello',
-        component: Hello
-    }],
+  routes: [{
+    path: '/user/presentations',
+    name: 'presentation',
+    component: presentations,
+  }],
 });
