76103e906edb806ffc65f2c0e0dd667c1d3b096f
Sachouille <sacha.perdrizat@gmail.com>
2020-04-23 23:43:33 +0200

    #109 create route to component

--- src/resources/js/router.js
+++ src/resources/js/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Hello from './components/Hello';
+import AccountEdition from './components/AccountEdit';
 
 Vue.use(VueRouter);
 
@@ -19,7 +20,13 @@ const router = new VueRouter({
       name: 'Test',
       icon: 'mdi-help-box',
       component: Hello,
-    }],
+    },
+    {
+      path: '/userEdit',
+      name: 'AccountEdition',
+      component: AccountEdition,
+    },
+  ],
 });
 
 export default router;
