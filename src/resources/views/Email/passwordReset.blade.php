62521ccba950e315846f8368ef729c57d039ff47
Fabio Marques <fabiomanuelsilvamarques@gmail.com>
2020-04-25 12:58:38 +0200

    #44 Reset password mail

---
+++ src/resources/views/Email/passwordReset.blade.php
@@ -0,0 +1,14 @@
+@component('mail::message')
+# Récupération de mot de passe
+
+Vous avez fait une demande de récupération de mot de passe, pour réinitialiser votre mot de passe veuillez cliquer sur le bouton ci-dessous.
+
+@component('mail::button', ['url' => config('app.url') . '/reset-password?token=' . $token])
+Réinitialiser mdp
+@endcomponent
+
+Si vous n'êtes pas à l'origine de cette demande merci de contacter nous contacter au plus vite.
+
+Merci d'utiliser nos services,<br>
+{{ config('app.name') }}
+@endcomponent
