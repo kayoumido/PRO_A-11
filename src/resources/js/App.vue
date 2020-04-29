1dd71e07682146c8f67b9ce216e5a1fba12ac9e2
Sachouille <sacha.perdrizat@gmail.com>
2020-04-29 09:59:27 +0200

    Revert "#90-rm-unwanted-files"
    This reverts commit 8f294d9abf87231aeda2286e6212015a8357d25d.

---
+++ src/resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app" class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="collapse navbar-collapse">
+                <div class="navbar-nav">
+                    <router-link to="/hello" class="nav-item nav-link">Home</router-link>
+                    <router-link to="/presentation" class="nav-item nav-link">presentation 1</router-link>
+                </div>
+            </div>
+        </nav>
+        <br/>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
