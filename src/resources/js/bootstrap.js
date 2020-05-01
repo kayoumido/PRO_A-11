412a9564ce270314523a5d2073deb424b163fd35
Fabio Marques <fabiomanuelsilvamarques@gmail.com>
2020-05-01 18:18:01 +0200

    #53-54 Moved Authorization header to the bootstrap file and added the default baseURL

--- src/resources/js/bootstrap.js
+++ src/resources/js/bootstrap.js
@@ -7,6 +7,8 @@
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common = { Authorization: `Bearer ${localStorage.getItem('Authorization-token')}` };
+window.axios.defaults.baseURL = '/api/v1';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
