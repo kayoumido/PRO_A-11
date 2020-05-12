41874622da3e5b201469ed71bcea0ec546b54518
Sachouille <sacha.perdrizat@gmail.com>
2020-05-12 22:38:18 +0200

    #37 add autocomplete behaviour

--- src/resources/js/components/layout/SearchBar.vue
+++ src/resources/js/components/layout/SearchBar.vue
@@ -2,13 +2,52 @@
     <v-autocomplete
         single-line
         hide-details
-        placeholder="Start typing to Search"
+        placeholder="Rechecher une presentation"
         append-icon="mdi-magnify"
+        hide-no-data
+        v-model="select"
+        :loading="loading"
+        :items="items"
+        item-value="id"
+        item-text="title"
+        :search-input.sync="search"
     ></v-autocomplete>
 </template>
 
 <script>
 export default {
   name: 'SearchBar',
+  data() {
+    return {
+      model: null,
+      value: '',
+      loading: false,
+      items: [],
+      search: null,
+      select: null,
+    };
+  },
+  beforeUpdate() {
+  },
+  watch: {
+    search(val) {
+      return val && val !== this.select && this.querySelections(val);
+    },
+  },
+  methods: {
+    querySelections(v) {
+      const apiUrl = '/presentations/search';
+      this.loading = true;
+      setTimeout(() => {
+        window.axios.get(apiUrl, { params: { keywords: v } })
+          .then((res) => {
+            this.items = res.data;
+          });
+      }, 500);
+
+      this.loading = false;
+    },
+  },
+
 };
 </script>
