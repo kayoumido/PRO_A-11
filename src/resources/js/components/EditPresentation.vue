690d1607c24222211d2f9ee4554753efbe0fc7ac
Lindwing <rui.lopesgouveia@heig-vd.ch>
2020-05-12 13:57:16 -0400

    #43 vuetify

---
+++ src/resources/js/components/EditPresentation.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-form
+        ref="form"
+        v-model="valid"
+        lazy-validation
+    >
+      <v-text-field
+            v-model="updatePresentationInfo.title"
+            :rules="titleRules"
+            label="Titre"
+      />
+
+      <v-text-field
+            v-model="updatePresentationInfo.date"
+            label="Date"
+      />
+
+      <v-btn
+            :disabled="!valid"
+            color="success"
+            class="mr-4"
+            @click="submitChange"
+            >
+            Appliquer
+      </v-btn>
+    </v-form>
+</template>
