f0bc6157197211db74f5e0ec2d23afbef4162439
Lindwing <rui.lopesgouveia@heig-vd.ch>
2020-05-20 19:17:52 +0200

    Update router.js

--- src/resources/js/router.js
+++ src/resources/js/router.js
@@ -9,6 +9,7 @@ import AccountEdition from './components/AccountEdit';
 import AccountCreation from './components/AccountCreation';
 import Reset from './components/Reset';
 import ResetPassword from './components/ResetPassword';
+import EditPresentation from './components/EditPresentation';
 import CreatePoll from './components/CreatePoll';
 
 Vue.use(VueRouter);
