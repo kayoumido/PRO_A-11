92f9036342c5d45719121a4c142d784ad3a61ff8
Fabio Marques <fabiomanuelsilvamarques@gmail.com>
2020-05-21 15:13:07 +0200

    #156 edit choice

--- src/resources/js/components/Choice.vue
+++ src/resources/js/components/Choice.vue
@@ -1,16 +1,29 @@
 <template>
     <v-sheet
         class="pa-12"
-        color="success">
-    {{choice.message}}
+        elevation="3"
+        color="grey lighten-2">
+        <div v-if="user_role !== 'presenter'">
+            {{choice.message}}
+        </div>
+        <div v-else>
+            <EditableText
+                :value="choice.message"
+                name="message"
+                :api-url="`/choices/${choice.id}`"></EditableText>
+        </div>
     </v-sheet>
 </template>
 
 <script>
+import EditableText from './EditableText';
+
 export default {
   name: 'Choice',
+  components: { EditableText },
   props: [
     'choice',
+    'user_role',
   ],
 };
 </script>
